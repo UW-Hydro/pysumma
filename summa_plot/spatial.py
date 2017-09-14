42984bd3f068e1de14c6b17bee7bca971a254206
arbennett <bennett.andr@gmail.com>
2017-09-13 23:54:22 -0700

    Further hovmoller implementation, cleanup.

--- summa_plot/spatial.py
+++ summa_plot/spatial.py
@@ -1,39 +1,39 @@
 '''Spatial plotting for SUMMA output'''
 
-import cartopy.feature as cfeature
-import cartopy.crs as ccrs
-from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
-import geopandas as gp
-import xarray as xr
+import shapely
 import numpy as np
+import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+from cartopy.feature import NaturalEarthFeature
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
-import shapely
 
 
 def add_map_features(ax, states_provinces=True, country_borders=True,
                      land=True, ocean=True, lake=False):
     '''Add background features to an axis'''
     if states_provinces:
-        states_provinces = cfeature.NaturalEarthFeature(category='cultural',
-                name='admin_1_states_provinces_lines', scale='50m', facecolor='none')
-        ax.add_feature(states_provinces, edgecolor='black', alpha=0.8, zorder=2)
+        states_provinces = NaturalEarthFeature(
+                category='cultural', name='admin_1_states_provinces_lines',
+                scale='50m', facecolor='none')
+        ax.add_feature(states_provinces, edgecolor='black', alpha=.8, zorder=2)
     if country_borders:
-        country_borders = cfeature.NaturalEarthFeature(category='cultural',
-                name='admin_0_boundary_lines_land', scale='50m', facecolor='none')
-        ax.add_feature(country_borders, edgecolor='black', zorder=2, linewidth=1)
+        ctry_borders = NaturalEarthFeature(
+            category='cultural', name='admin_0_boundary_lines_land',
+            scale='50m', facecolor='none')
+        ax.add_feature(ctry_borders, edgecolor='black', zorder=2, linewidth=1)
     if land:
-        land = cfeature.NaturalEarthFeature(category='physical',
-            name='land', scale='50m', facecolor='gray')
+        land = NaturalEarthFeature(
+            category='physical', name='land', scale='50m', facecolor='gray')
         ax.add_feature(land, facecolor='lightgray', zorder=0)
     if ocean:
-        ocean = cfeature.NaturalEarthFeature(category='physical',
-            name='ocean', scale='50m', facecolor='blue')
+        ocean = NaturalEarthFeature(
+            category='physical', name='ocean', scale='50m', facecolor='blue')
         ax.add_feature(ocean, facecolor='lightblue', zorder=1)
     if lake:
-        rivers_lakes = cfeature.NaturalEarthFeature(category='physical',
-            name='rivers_lake_centerlines', scale='50m', facecolor='none')
+        rivers_lakes = NaturalEarthFeature(
+            category='physical', name='rivers_lake_centerlines',
+            scale='50m', facecolor='none')
         ax.add_feature(rivers_lakes, facecolor='lightblue', zorder=2)
 
 
@@ -45,12 +45,14 @@ def gen_patches(data_array, geodf, simplify_level=500):
     for val, shp in zip(data_array.values, geodf.geometry):
         if isinstance(shp, shapely.geometry.MultiPolygon):
             for sub in shp:
-                patches.append(Polygon(np.asarray(sub.simplify(simplify_level).exterior)))
+                patches.append(Polygon(np.asarray(
+                    sub.simplify(simplify_level).exterior)))
                 vals.append(val)
         else:
-            patches.append(Polygon(np.asarray(shp.simplify(simplify_level).exterior)))
+            patches.append(
+                    Polygon(np.asarray(shp.simplify(simplify_level).exterior)))
             vals.append(val)
-    patches = PatchCollection(patches, linewidth=0.0, edgecolor=None, alpha=1.0)
+    patches = PatchCollection(patches, linewidth=0., edgecolor=None, alpha=1.0)
     patches.set_array(np.array(vals))
     return patches
 
@@ -67,40 +69,60 @@ def spatial(data_array, geodf, simplify_level=500, proj=ccrs.Mercator()):
 
 
 def hovmuller(data_array, xdim, ydim, how=None, cmap='viridis'):
+    '''Make a Hovmuller plot'''
     # Check if dimensions are valid
+    fig, ax = plt.subplots(nrows=1, ncols=1)
     time_groups = ['year', 'month', 'day', 'hour',
                    'minute', 'second', 'dayofyear',
                    'week', 'dayofweek', 'weekday', 'quarter']
     x_da_dim = xdim in list(data_array.dims)
     x_tg_dim = 'time.{}'.format(xdim) in time_groups
-    if x_da_dim:
-        continue
-    elif x_tg_dim:
+    if x_tg_dim:
         xdim = 'time.{}'.format(xdim)
         if not how:
             raise Exception("Must specify aggregation "
                             "method for x dimension")
-    else:
+    elif not x_da_dim:
         raise Exception("x dimension not valid")
 
     y_da_dim = ydim in list(data_array.dims)
     y_tg_dim = 'time.{}'.format(ydim) in time_groups
-    if y_da_dim:
-        continue
-    elif y_tg_dim:
+    if y_tg_dim:
         ydim = 'time.{}'.format(ydim)
         if not how:
             raise Exception("Must specify aggregation "
                             "method for y dimension")
-    else:
+    elif not y_da_dim:
         raise Exception("y dimension not valid")
 
     # Do the group statements
-    how_dict = {'mean'   : lambda x: x.mean(),
-                'max'    : lambda x: x.max(),
-                'min'    : lambda x: x.min(),
-                'median' : lambda x: x.median(),
-                'std'    : lambda x: x.std()}
-    if how and (x_tg_dim or y_tg_dim):
-        continue
+    how_dict = {'mean': lambda x: x.mean(),
+                'max': lambda x: x.max(),
+                'min': lambda x: x.min(),
+                'median': lambda x: x.median(),
+                'std': lambda x: x.std()}
+    if how and how not in how_dict.keys():
+        raise Exception("Invalid time aggregation method given")
 
+    # Three cases now:
+    #   1. Both x and y axes have time grouped dimensions
+    #   2. One of x or y have a time grouped dimension
+    #   3. Neither axis use time grouped dimensions
+    #
+    # TODO: Check that the dimenstions of the data array match
+    #       for each of these cases
+    if how and x_tg_dim and y_tg_dim:
+        grouped = how_dict[how](data_array.groupby(
+            [lambda x: x[x_tg_dim], lambda x: x[y_tg_dim]])).unstack()
+    elif how and x_tg_dim:
+        grouped = how_dict[how](data_array.groupby(lambda x: x[x_tg_dim]))
+    elif how and y_tg_dim:
+        grouped = how_dict[how](data_array.groupby(lambda x: x[y_tg_dim]))
+    else:
+        pass
+
+    x = np.asarray(grouped.axes[1])
+    y = np.asarray(grouped.axes[0])
+    ax.axes.pcolormesh(x, y, grouped.values, cmap=cmap)
+    ax.axes.axis([x.min(), x.max(), y.min(), y.max()])
+    return fig, ax
