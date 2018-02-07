7da47bdae2731c024dc4ffdba5fd488f70b72a8f
YOUNGDON CHOI <yc5ef@virginia.edu>
2018-02-07 12:05:35 -0500

    change url and license in setup.py

--- pysumma/setup.py
+++ pysumma/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(name='pysumma',
       version = '0.1',
       description = 'This is pySUMMA',
-      url = 'https://github.com/DavidChoi76/pysumma_alpha',
+      url = 'https://github.com/UW-Hydro/pysumma',
       author = 'YoungDon Choi',
       author_email = 'yc5ef@virginia.edu',
-      license = 'MIT',
+      license = 'BSD-3-Clause',
       packages=['pysumma'],
       test_suite = 'tests')
 #      setup_requires=['pytest-runner', 'xarray'],
