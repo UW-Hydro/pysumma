5da379fd9da92f526f12e83ef64dd0ffa59bf36b
Unknown <edwardm72@gmail.com>
2018-03-02 23:40:47 -0500

    Merged Proposed changes
    Changed ProposedDecisions and ProposedSimulation to Decisions and Simulation

--- pysumma/pysumma/Simulation.py
+++ pysumma/pysumma/Simulation.py
@@ -1,13 +1,16 @@
 #from Decisions import Decisions         # This is for testing in cmd window.
+from pysumma.Option import Option
 from .Decisions import Decisions       # This is for testing in this python code.
 import subprocess
 import os
 
+
 class Simulation:
     executable = ''
     run_suffix = '_'
+
     def __init__(self, filepath):
-        self.filepath = os.path.abspath(filepath)
+        self.filepath = filepath
         self.setting_path = FileManagerOption('setting_path', self.filepath)
         self.input_path = FileManagerOption('input_path', self.filepath)
         self.output_path = FileManagerOption('output_path', self.filepath)
@@ -37,64 +40,55 @@ class Simulation:
             cmd = "{} -p never -s {}       -m {}".format(self.executable, self.run_suffix, self.filepath)
             subprocess.run(cmd, shell=True)
 
-class FileManagerOption:
-    def __init__(self, name, filepath):
-        self.name = name
-        self.file_manager_filepath = filepath
-        self.text = self.open_read()
 
-    def open_read(self):
-        with open(self.file_manager_filepath, 'rt') as f:
-            return f.readlines()
+class FileManagerOption(Option):
+    # key_position is the position in line.split() where the key name is
+    # value_position is the position in line.split() where the value is
+    # By default, delimiter=None, but can be set to split each line on different characters
+    def __init__(self, name, file_manager_filepath):
+        super().__init__(name, file_manager_filepath, key_position=2, value_position=0, delimiter=None)
 
-    def get_line_no(self, name):
-        for line_no, line_contents in enumerate(self.text):
-            filepath_filename = line_contents.split("'")
-            name1 = filepath_filename[2].split(" ")[-1].strip()
-            if name1 == name:
-                return line_no, line_contents
+    '''
+        value is the thing read from the Simulation file-a filepath with or without a trailing '/'
+        filepath adds a '/' to the value, if needed (does not contain the filename?)
+        filename is the last part of the value-only the filename
+        value = filepath + filename
+    '''
 
-    def get_value(self):
-        self.line_no, self.line_contents = self.get_line_no(self.name)
-        words = self.line_contents.split("'")
-        words = [w.strip() for w in words if w.strip() != "" and w.strip() != "!"]
-        return words[0]
-
-    def write_value(self, new_value):
-        self.text[self.line_no] = self.line_contents.replace(self.value, new_value, 1)
-        self.edit_save()
-
-    def edit_save(self):
-        with open(self.file_manager_filepath, 'wt') as f:
-            f.writelines(self.text)
-
+    # TODO: Do we actually need a value and a filepath? What if we just added a trailing '/' to the value automatically?
     @property
     def value(self):
         return self.get_value()
 
-
     @value.setter
     def value(self, new_value):
-        self.write_value(new_value)
+        self.write_value(old_value=self.value, new_value=new_value)
 
+    # TODO: What does this do? it just puts a '/' at the end of the value?
+    # filepath is the path up to the filename, not including it
     @property
     def filepath(self):
+        print("value: " + self.value)
         if not self.value.endswith('/'):
             return "/".join(self.value.split('/')[:-1]) + "/"
         else:
             return self.value
 
     @filepath.setter
-    def filepath(self, new_value):
-        value = new_value + self.filename
-        self.write_value(value)
+    def filepath(self, new_filepath):
+        value = new_filepath + self.filename
+        self.write_value(old_value=self.value, new_value=value)
 
+    # TODO: Do we want to just use Unix file URLS (dir/dir/file) or also Windows (dir\dir\file)?
+    # Returns the file name of the FileManagerOption
     @property
     def filename(self):
-        return self.value.split('/')[-1]
+        if self.value.endswith('/'):
+            return self.value.split('/')[-2]
+        else:
+            return self.value.split('/')[-1]
 
     @filename.setter
-    def filename(self, new_value):
-        value = self.filepath + new_value
-        self.write_value(value)
-
+    def filename(self, new_filename):
+        value = self.filepath + new_filename
+        self.write_value(old_value=self.value, new_value=value)
