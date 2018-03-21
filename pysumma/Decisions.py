b326f63f097498f4afb43fe3cfd20f37774bbb12
YOUNGDON CHOI <yc5ef@virginia.edu>
2018-03-21 16:39:20 -0400

    updated code to write and save for decision.

--- pysumma/Decisions.py
+++ pysumma/Decisions.py
@@ -1,51 +1,57 @@
+import weakref
 class Decisions:
     def __init__(self, filepath):
         self.filepath = filepath
-        self.simulStart = SimulDatetime('simulStart', self.filepath)
-        self.simulFinsh = SimulDatetime('simulFinsh', self.filepath)
-        self.soilCatTbl = DecisionOption('soilCatTbl', self.filepath)
-        self.vegeParTbl = DecisionOption('vegeParTbl', self.filepath)
-        self.soilStress = DecisionOption('soilStress', self.filepath)
-        self.stomResist = DecisionOption('stomResist', self.filepath)
-        self.num_method = DecisionOption('num_method', self.filepath)
-        self.fDerivMeth = DecisionOption('fDerivMeth', self.filepath)
-        self.LAI_method = DecisionOption('LAI_method', self.filepath)
-        self.f_Richards = DecisionOption('f_Richards', self.filepath)
-        self.groundwatr = DecisionOption('groundwatr', self.filepath)
-        self.hc_profile = DecisionOption('hc_profile', self.filepath)
-        self.bcUpprTdyn = DecisionOption('bcUpprTdyn', self.filepath)
-        self.bcLowrTdyn = DecisionOption('bcLowrTdyn', self.filepath)
-        self.bcUpprSoiH = DecisionOption('bcUpprSoiH', self.filepath)
-        self.bcLowrSoiH = DecisionOption('bcLowrSoiH', self.filepath)
-        self.veg_traits = DecisionOption('veg_traits', self.filepath)
-        self.canopyEmis = DecisionOption('canopyEmis', self.filepath)
-        self.snowIncept = DecisionOption('snowIncept', self.filepath)
-        self.windPrfile = DecisionOption('windPrfile', self.filepath)
-        self.astability = DecisionOption('astability', self.filepath)
-        self.canopySrad = DecisionOption('canopySrad', self.filepath)
-        self.alb_method = DecisionOption('alb_method', self.filepath)
-        self.compaction = DecisionOption('compaction', self.filepath)
-        self.snowLayers = DecisionOption('snowLayers', self.filepath)
-        self.thCondSnow = DecisionOption('thCondSnow', self.filepath)
-        self.thCondSoil = DecisionOption('thCondSoil', self.filepath)
-        self.spatial_gw = DecisionOption('spatial_gw', self.filepath)
-        self.subRouting = DecisionOption('subRouting', self.filepath)
-
-class DecisionOption:
-    def __init__(self, name, filepath):
-        self.filepath = filepath
-        self.text = self.open_read()
-        self.name = name
-        self.line_no, self.line_contents = self.get_line_no(self.name)
-        self.get_description()
-        self.options = self.get_options()
+        self.file_contents = self.open_read()
+        self.simulStart = SimulDatetime(self, 'simulStart')
+        self.simulFinsh = SimulDatetime(self, 'simulFinsh')
+        self.soilCatTbl = DecisionOption(self, 'soilCatTbl')
+        self.vegeParTbl = DecisionOption(self, 'vegeParTbl')
+        self.soilStress = DecisionOption(self, 'soilStress')
+        self.stomResist = DecisionOption(self, 'stomResist')
+        self.num_method = DecisionOption(self, 'num_method')
+        self.fDerivMeth = DecisionOption(self, 'fDerivMeth')
+        self.LAI_method = DecisionOption(self, 'LAI_method')
+        self.f_Richards = DecisionOption(self, 'f_Richards')
+        self.groundwatr = DecisionOption(self, 'groundwatr')
+        self.hc_profile = DecisionOption(self, 'hc_profile')
+        self.bcUpprTdyn = DecisionOption(self, 'bcUpprTdyn')
+        self.bcLowrTdyn = DecisionOption(self, 'bcLowrTdyn')
+        self.bcUpprSoiH = DecisionOption(self, 'bcUpprSoiH')
+        self.bcLowrSoiH = DecisionOption(self, 'bcLowrSoiH')
+        self.veg_traits = DecisionOption(self, 'veg_traits')
+        self.canopyEmis = DecisionOption(self, 'canopyEmis')
+        self.snowIncept = DecisionOption(self, 'snowIncept')
+        self.windPrfile = DecisionOption(self, 'windPrfile')
+        self.astability = DecisionOption(self, 'astability')
+        self.canopySrad = DecisionOption(self, 'canopySrad')
+        self.alb_method = DecisionOption(self, 'alb_method')
+        self.compaction = DecisionOption(self, 'compaction')
+        self.snowLayers = DecisionOption(self, 'snowLayers')
+        self.thCondSnow = DecisionOption(self, 'thCondSnow')
+        self.thCondSoil = DecisionOption(self, 'thCondSoil')
+        self.spatial_gw = DecisionOption(self, 'spatial_gw')
+        self.subRouting = DecisionOption(self, 'subRouting')
 
     def open_read(self):
         with open(self.filepath, 'rt') as f:
             return f.readlines()
 
+class DecisionOption:
+    def __init__(self, parent, name):
+        self.parent =parent
+        # self.text = parent.file_contents
+        self.name = name
+        self.line_no, self.line_contents = self.get_line_no(self.name)
+        self.get_description()
+        self.options = self.get_options()
+
+    # def open_read(self):
+    #     with open(self.filepath, 'rt') as f:
+    #         return f.readlines()
+
     def get_line_no(self, text_startwith):
-        for line_no, line_contents in enumerate(self.text):
+        for line_no, line_contents in enumerate(self.parent.file_contents):
             if line_contents.split()[0].startswith(text_startwith):
                 return line_no, line_contents
 
@@ -62,22 +68,27 @@ class DecisionOption:
     def get_options(self):
         start_line = 43
         option_list = []
-        for num, line_contents in enumerate(self.text[start_line:]):
+        for num, line_contents in enumerate(self.parent.file_contents[start_line:]):
             line_num = num + start_line
             if line_contents.startswith('! ({})'.format(self.option_number)):
-                while self.text[line_num+1].find("---") < 0 and self.text[line_num+1].find("****") < 0:
+                while self.parent.file_contents[line_num+1].find("---") < 0 and self.parent.file_contents[line_num+1].find("****") < 0:
                     line_num += 1
-                    option_list.append(self.text[line_num].split('!')[1].strip())
+                    option_list.append(self.parent.file_contents[line_num].split('!')[1].strip())
                 else:
                     return option_list
 
     def write_value(self, new_value):
-        self.text[self.line_no] = self.line_contents.replace(self.value, new_value, 1)
+        self.parent.file_contents[self.line_no] = self.line_contents.replace(self.value, new_value, 1)
+        # print (self.parent.file_contents[self.line_no])
+        # self.parent.file_contents[self.line_no] = self.line_contents.replace(self.value, new_value, 1)
+        # print (self.line_contents.replace(self.value, new_value, 1))
+
         self.edit_save()
 
     def edit_save(self):
-        with open(self.filepath, 'wt') as f:
-            f.writelines(self.text)
+        with open(self.parent.filepath, 'wt') as f:
+            f.writelines(self.parent.file_contents)
+            # S = Simulation(att)
 
     @property
     def value(self):
