4bdf1ae86fa4f40b6b40413eabfadbf116980aef
DavidChoi76 <yc5ef@virginia.edu>
2018-03-22 21:58:42 -0400

    #18 update utils.py to interact with HS, and update notebook

--- pysumma/utils.py
+++ pysumma/utils.py
@@ -2,19 +2,26 @@ import shutil
 import os
 from urllib.request import urlretrieve
 import subprocess
+from hs_restclient import HydroShare, HydroShareAuthBasic
 
+class utils():
 
-def install_test_cases(directory):
-    url = "https://ral.ucar.edu/sites/default/files/public/projects/structure-for-unifying-multiple-modeling-alternatives-summa/summatestcases-2.x.tar.gz"
-    filepath = directory + "summatestcases-2.x.tar.gz"
-    urlretrieve(url, filepath)
-    shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
+    def __init__(self, hs):
+        self.hs = hs
 
-    cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(directory)
-    subprocess.run(cmd, shell=True)
+    def install_test_cases(self, directory):
+        url = "https://ral.ucar.edu/sites/default/files/public/projects/structure-for-unifying-multiple-modeling-alternatives-summa/summatestcases-2.x.tar.gz"
+        filepath = directory + "summatestcases-2.x.tar.gz"
+        urlretrieve(url, filepath)
+        shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
+        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(directory)
+        subprocess.run(cmd, shell=True)
 
-# dir = '/work/'
-
-# install_test_cases(dir)
-
-# install_local()
+    def install_test_cases_hs(self, resource_id, save_filepath):
+        self.hs.getResource(resource_id, destination = save_filepath, unzip = True)
+        resource_name = 'summatestcases-2.x.tar.gz'
+        testcase_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
+        shutil.unpack_archive(testcase_filepath, extract_dir=os.path.dirname(testcase_filepath))
+        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(os.path.dirname(testcase_filepath))
+        subprocess.run(cmd, shell=True)
+        return  os.path.dirname(testcase_filepath)
