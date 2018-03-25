48ed4b3700b0502b0e38794d5fc091a163c5091e
arbennett <bennett.andr@gmail.com>
2018-03-25 15:33:54 -0700

    Cleanup. Add better data filtering for spatial plot.
    Now users can specify the columns/dimensions to use to align the shapefile and
    output data.  Use the da_coord and gdf_coord keyword arguments to choose these.

--- summa_plot/__init__.py
+++ summa_plot/__init__.py
@@ -1,6 +1,7 @@
 
+from .utils import add_map_features, simplify_shapefile
 from .layers import layers
 from .plot import *
-from .spatial import spatial, add_map_features
+from .spatial import spatial, gen_patches
 from .hovmoller import hovmoller
 from .timeseries import *
