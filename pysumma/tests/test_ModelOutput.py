8c41d670f98a8c6b2879941d07ab4f6498ed1caa
edwmorgan <edwardm72@gmail.com>
2018-03-26 01:26:27 -0400

    ModelOutput now reads and writes variables with tests

--- pysumma/tests/test_ModelOutput.py
+++ pysumma/tests/test_ModelOutput.py
@@ -1,6 +1,7 @@
 from pysumma.ModelOutput import ModelOutput
 import unittest
 import random
+from shutil import copyfile
 import os
 
 
@@ -8,42 +9,55 @@ class TestModelOutput(unittest.TestCase):
     my_path = os.path.abspath(os.path.dirname(__file__)) + '/meta/'
     filename = 'Model_Output.txt'
     filepath = os.path.join(my_path, filename)
-    filepath2 = os.path.join(my_path, filename)
+    filename2 = 'tmp_{}'.format(filename)
+    filepath2 = os.path.join(my_path, filename2)
+    copyfile(filepath, filepath2)
     mo = ModelOutput(filepath2)
-    print("filepath: " + filepath + '\n')
 
     def read_vars(self):
         var_list = []
-        with open(self.filepath, 'r') as file:
+        with open(self.filepath2, 'r') as file:
             for line in file:
                 if len(line.split("|")) > 1:
                     var_list.append(line.split("|")[0].strip())
         return var_list
 
     # Does ModelOutput read all the variables from the file?
-    def test_read_variables(self):
+    def test_1_read_variables(self):
         assert self.mo.read_variables_from_file() == self.read_vars()
 
-    # Does ModelOutput accurately check for the variable?
-    def test_check_for_variables(self):
-        for var in self.read_vars():
-            pass
-        # TODO: check for every variable in the file, then check for variables not in the file (all others in mo.var_list
+    # Go through all possible variables-does ModelOutput correctly determine their status?
+    def test_2_check_for_variables(self):
+        vars_in_file = self.read_vars()
+        for var in self.mo.var_choices:
+            if var in vars_in_file:
+                assert self.mo.check_for_variable(var) is True, "var: " + var + " should be in the file."
+            else:
+                assert self.mo.check_for_variable(var) is False, "var: " + var + " should not be in the file"
 
     # Upon adding every possible variable, does ModelOutput reflect the changes?
-    # def test_add_all_valid_variables(self):
-    #     for i in range(0, len(self.mo.var_choices)):
-    #         self.mo.write_variable_to_file(self.mo.var_choices[i])
-    #         assert self.mo.var_choices[i] in self.read_vars()
+    def test_3_add_all_valid_variables(self):
+        for i in range(0, len(self.mo.var_choices)):
+            self.mo.write_variable_to_file(self.mo.var_choices[i])
+            assert self.mo.var_choices[i] in self.read_vars()
 
-    # Upon removing
-    # def test_remove_all_valid_variables(self):
-    #     for i in range(0, len(self.mo.var_choices)):
-    #         self.mo.remove_variable(self.mo.var_choices[i])
-    #         assert self.mo.var_choices[i] not in self.read_vars()
+     # Upon removing every variable in the
+    def test_4_remove_all_valid_variables(self):
+        for i in range(0, len(self.mo.var_choices)):
+            self.mo.remove_variable(self.mo.var_choices[i])
+            assert self.mo.read_variables_from_file() == self.read_vars()
+            assert self.mo.var_choices[i] not in self.read_vars()
 
-    # def test_check_for_variables(self):
-    #
+    # Testing a 'normal' situation-when someone adds just one variable to a file, checks it, then deletes it
+    def test_5_pptrate(self):
+        self.mo.write_variable_to_file('pptrate')
+        assert self.mo.check_for_variable('pptrate') is True
+        self.mo.write_variable_to_file('pptrate')
+        assert self.mo.read_variables_from_file() == ['pptrate']
+        self.mo.remove_variable('nSoil')
+        assert self.mo.read_variables_from_file() == ['pptrate']
+        self.mo.remove_variable('pptrate')
+        assert self.mo.read_variables_from_file() == []
     # def test_add_invalid_variables(self):
     #
     # def test_add_duplicate_variables(self):
