4e5fa2f920da97b9c672af404fbac6528b1cb1b6
Youngdon Choi <choiyd1115@gmail.com>
2018-03-30 13:39:34 -0400

    chagne code for download from HS and ucar web

--- pysumma/utils.py
+++ pysumma/utils.py
@@ -6,10 +6,7 @@ from hs_restclient import HydroShare, HydroShareAuthBasic
 
 class utils():
 
-    def __init__(self, hs):
-        self.hs = hs
-
-    def install_test_cases(self, directory):
+    def install_test_cases(directory):
         url = "https://ral.ucar.edu/sites/default/files/public/projects/structure-for-unifying-multiple-modeling-alternatives-summa/summatestcases-2.x.tar.gz"
         filepath = directory + "summatestcases-2.x.tar.gz"
         urlretrieve(url, filepath)
@@ -17,8 +14,9 @@ class utils():
         cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(directory)
         subprocess.run(cmd, shell=True)
 
-    def install_test_cases_hs(self, resource_id, save_filepath):
-        self.hs.getResource(resource_id, destination = save_filepath, unzip = True)
+    def install_test_cases_hs(hs, resource_id, save_filepath):
+        authen = hs
+        authen.getResource(resource_id, destination = save_filepath, unzip = True)
         resource_name = 'summatestcases-2.x.tar.gz'
         testcase_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
         shutil.unpack_archive(testcase_filepath, extract_dir=os.path.dirname(testcase_filepath))
