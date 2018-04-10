99d25047771b467e5c29a121357a5914fbf46766
arbennett <bennett.andr@gmail.com>
2018-04-10 16:37:22 -0700

    Moving things around

--- pysumma/Simulation.py
+++ pysumma/Simulation.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 from pysumma.Option import Option
 from pysumma.Decisions import Decisions
 import subprocess
@@ -5,39 +7,10 @@ import os
 import xarray as xr
 
 
-class Simulation:
-    def __init__(self, filepath):
-        self.filepath = os.path.abspath(filepath)
-        self.file_dir = os.path.dirname(self.filepath)
-        self.file_contents = self.open_read()
-        self.fman_ver = FileManagerOption(self, 'fman_ver')
-        self.setting_path = FileManagerOption(self, 'setting_path')
-        self.input_path = FileManagerOption(self, 'input_path')
-        self.output_path = FileManagerOption(self, 'output_path')
-        self.decision_path = FileManagerOption(self, 'decision')
-        self.meta_time = FileManagerOption(self, 'meta_time')
-        self.meta_attr = FileManagerOption(self, 'meta_attr')
-        self.meta_type = FileManagerOption(self, 'meta_type')
-        self.meta_force = FileManagerOption(self, 'meta_force')
-        self.meta_localpar = FileManagerOption(self, 'meta_localpar')
-        self.OUTPUT_CONTROL = FileManagerOption(self, 'output_control')
-        self.meta_index = FileManagerOption(self, 'meta_index')
-        self.meta_basinpar = FileManagerOption(self, 'meta_basinpar')
-        self.meta_basinvar = FileManagerOption(self, 'meta_basinvar')
-        self.local_attr = FileManagerOption(self, 'local_attr')
-        self.local_par = FileManagerOption(self, 'local_par')
-        self.basin_par = FileManagerOption(self, 'basin_par')
-        self.forcing_list = FileManagerOption(self, 'forcing_list')
-        self.initial_cond = FileManagerOption(self, 'initial_cond')
-        self.para_trial = FileManagerOption(self, 'para_trial')
-        self.output_prefix = FileManagerOption(self, 'output_prefix')
-        self.decision_obj = Decisions(self.setting_path.value + self.decision_path.value)
-
-    def open_read(self):
-        # read filemanager text file
-        with open(self.filepath, 'rt') as f:
-            # read every line of filemanager and return as list format
-            return f.readlines()
+class Simulation(object):
+    def __init__(self, file_manager_path):
+        self.file_manager = FileManager(filepath_manager_path)
+        self.decisions = Decisions(self.file_manager.decisions_file)
 
     def execute(self, run_suffix, run_option):
         self.run_suffix = run_suffix
@@ -74,43 +47,108 @@ class Simulation:
                         self.run_suffix + '_timestep.nc'
         return xr.open_dataset(out_file_path), out_file_path
 
+    def run_local(self, **kwargs):
+        pass
 
-class FileManagerOption(Option):
+    def run_docker(self, **kwargs):
+        pass
 
-    # key_position is the position in line.split() where the key name is
-    # value_position is the position in line.split() where the value is
-    # By default, delimiter=None, but can be set to split each line on different characters
-    def __init__(self, parent, name):
-        super().__init__(name, parent, key_position=1, value_position=0, delimiter="!")
+    def run_remote(self, **kwargs):
+        raise NotImplementedError()
 
-    @property
-    def value(self):
-        return self.get_value()
 
-    @value.setter
-    def value(self, new_value):
-        self.write_value(old_value=self.value, new_value=new_value)
+class FileManager(object):
 
-    # filepath is the path up to the filename, not including it
-    @property
-    def filepath(self):
-        if not self.value.endswith('/'):
-            return "/".join(self.value.split('/')[:-1]) + "/"
-        else:
-            return self.value
+    fileman_opts = ['header', 'settings_path', 'input_path', 'output_path',
+                   'decisions_path', 'meta_time', 'meta_attr', 'meta_type',
+                   'meta_force', 'meta_local_param', 'output_control',
+                   'meta_local_idx', 'meta_basin_param', 'meta_basin_var',
+                   'local_attr', 'local_param_info', 'basin_param_info',
+                   'force_file_list', 'model_init_cond', 'param_trial',
+                   'output_prefix']
+    original_file_contents = None
+    original_path = None
+    overwrite_lines = []
 
-    # Replace the filepath in the value in fileManager.txt
-    @filepath.setter
-    def filepath(self, new_filepath):
-        value = new_filepath + self.filename
-        self.write_value(old_value=self.value, new_value=value)
+    def __init__(self, path):
+        self.original_path = path
+        self.read(path)
 
-    # Returns the file name of the FileManagerOption
-    @property
-    def filename(self):
-        return self.value.split('/')[-1]
+    def read(self, path):
+        with open(path, 'r') as f:
+            self.original_contents = f.readlines()
+        opt_count = 0
+        attrs = {}
+        for linum, line in enumerate(self.original_contents):
+            if not line.startswith('!'):
+                attrs[fileman_opts[opt_count]] = line.split('!')[0].strip()
+                opt_count += 1
+                self.overwrite_lines.append(linum)
+        assert opt_count == len(fileman_opts)
+        self.__dict__.update(attrs)
 
-    @filename.setter
-    def filename(self, new_filename):
-        value = self.filepath + new_filename
-        self.write_value(old_value=self.value, new_value=value)
+    def write(self, path=None):
+        assert original_file_contents is not None
+        if not path:
+            path = self.original_path
+        opt_count = 0
+        self.new_contents = []
+        for linum, line in enumerate(self.original_contents):
+            if linum in self.overwrite_lines:
+                oldval = line.split('!')[0].strip()
+                newval = self.__dict__[fileman_opts[opt_count]]
+                line = line.replace(oldval, newval)
+                opt_count += 1
+            self.new_contents.append(line)
+        assert opt_count == len(self.overwrite_lines)
+        with open(path, 'w') as f:
+            for line in self.new_contents:
+                f.write(line)
+
+
+class Decisions(object):
+
+    def __init__(self, path):
+        pass
+
+    def read(self, path):
+        pass
+
+    def write(self, path):
+        pass
+
+
+class OutputControl(object):
+
+    def __init__(self, path):
+        pass
+
+    def read(self, path):
+        pass
+
+    def write(self, path):
+        pass
+
+
+class BasinParameters(object):
+
+    def __init__(self, path):
+        pass
+
+    def read(self, path):
+        pass
+
+    def write(self, path):
+        pass
+
+
+class LocalParameters(object):
+
+    def __init__(self, path):
+        pass
+
+    def read(self, path):
+        pass
+
+    def write(self, path):
+        pass
