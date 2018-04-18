29bd91d89efe4949d9d23394eb96544ba5e962c8
arbennett <bennett.andr@gmail.com>
2018-04-18 11:00:55 -0700

    PEP8 fixes

--- pysumma/Simulation.py
+++ pysumma/Simulation.py
@@ -1,51 +1,65 @@
-from collections import namedtuple
 
-from pysumma.Option import Option
-from pysumma.Decisions import Decisions
 import subprocess
-import os
-import xarray as xr
 
 
 class Simulation(object):
     def __init__(self, file_manager_path):
-        self.file_manager = FileManager(filepath_manager_path)
+        self.file_manager = FileManager(file_manager_path)
         self.decisions = Decisions(self.file_manager.decisions_file)
+        self.output_control = OutputControl(self.file_manager.output_control)
 
     def execute(self, run_suffix, run_option):
         self.run_suffix = run_suffix
         if run_option == 'local':
-            cmd = "{} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+            cmd = "{} -p never -s {} -m {}".format(self.executable,
+                                                   self.run_suffix,
+                                                   self.filepath)
 
         elif run_option == "docker_latest":
             self.executable = 'bartnijssen/summa:latest'
-            cmd = "docker run -v {}:{}".format(self.file_dir, self.file_dir) + \
-                  " -v {}:{}".format(self.setting_path.filepath, self.setting_path.filepath) + \
-                  " -v {}:{}".format(self.input_path.filepath, self.input_path.filepath) + \
-                  " -v {}:{}".format(self.output_path.filepath, self.output_path.filepath) + \
-                  " {} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+            cmd = "docker run -v {}:{}".format(self.file_dir,
+                                               self.file_dir) + \
+                  " -v {}:{}".format(self.setting_path.filepath,
+                                     self.setting_path.filepath) + \
+                  " -v {}:{}".format(self.input_path.filepath,
+                                     self.input_path.filepath) + \
+                  " -v {}:{}".format(self.output_path.filepath,
+                                     self.output_path.filepath) + \
+                  " {} -p never -s {} -m {}".format(self.executable,
+                                                    self.run_suffix,
+                                                    self.filepath)
 
         elif run_option == "docker_develop":
             self.executable = 'bartnijssen/summa:develop'
-            cmd = "docker run -v {}:{}".format(self.file_dir, self.file_dir) + \
-                  " -v {}:{}".format(self.setting_path.filepath, self.setting_path.filepath) + \
-                  " -v {}:{}".format(self.input_path.filepath, self.input_path.filepath) + \
-                  " -v {}:{}".format(self.output_path.filepath, self.output_path.filepath) + \
-                  " {} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+            cmd = "docker run -v {}:{}".format(self.file_dir,
+                                               self.file_dir) + \
+                  " -v {}:{}".format(self.setting_path.filepath,
+                                     self.setting_path.filepath) + \
+                  " -v {}:{}".format(self.input_path.filepath,
+                                     self.input_path.filepath) + \
+                  " -v {}:{}".format(self.output_path.filepath,
+                                     self.output_path.filepath) + \
+                  " {} -p never -s {} -m {}".format(self.executable,
+                                                    self.run_suffix,
+                                                    self.filepath)
 
         else:
-            raise ValueError('No executable defined. Set as "executable" attribute of Simulation or check run_option')
+            raise ValueError('No executable defined. Set as'
+                             '"executable" attribute of Simulation'
+                             'or check run_option')
         # run shell script in python
         if self.library_path:
-            cmd = "".join(['export LD_LIBRARY_PATH="{}";'.format(self.library_path),
-                          cmd])
-        proc = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            libstr = 'export LD_LIBRARY_PATH="{}";'.format(self.library_path)
+            cmd = "".join([libstr, cmd])
+        proc = subprocess.Popen(cmd, shell=True,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         # define output file name
-        out_file_path = self.output_path.filepath + \
-                        self.output_prefix.value + 'output_' + \
-                        self.run_suffix + '_timestep.nc'
-        return xr.open_dataset(out_file_path), out_file_path
+        self.output_file = "".join([self.output_path.filepath,
+                                    self.output_prefix.value + 'output_',
+                                    self.run_suffix + '_timestep.nc'])
+        return proc
 
     def run_local(self, **kwargs):
         pass
@@ -59,13 +73,13 @@ class Simulation(object):
 
 class FileManager(object):
 
-    fileman_opts = ['header', 'settings_path', 'input_path', 'output_path',
-                   'decisions_path', 'meta_time', 'meta_attr', 'meta_type',
-                   'meta_force', 'meta_local_param', 'output_control',
-                   'meta_local_idx', 'meta_basin_param', 'meta_basin_var',
-                   'local_attr', 'local_param_info', 'basin_param_info',
-                   'force_file_list', 'model_init_cond', 'param_trial',
-                   'output_prefix']
+    fman_opts = ['header', 'settings_path', 'input_path', 'output_path',
+                 'decisions_path', 'meta_time', 'meta_attr', 'meta_type',
+                 'meta_force', 'meta_local_param', 'output_control',
+                 'meta_local_idx', 'meta_basin_param', 'meta_basin_var',
+                 'local_attr', 'local_param_info', 'basin_param_info',
+                 'force_file_list', 'model_init_cond', 'param_trial',
+                 'output_prefix']
     original_file_contents = None
     original_path = None
     overwrite_lines = []
@@ -81,14 +95,14 @@ class FileManager(object):
         attrs = {}
         for linum, line in enumerate(self.original_contents):
             if not line.startswith('!'):
-                attrs[fileman_opts[opt_count]] = line.split('!')[0].strip()
+                attrs[self.fman_opts[opt_count]] = line.split('!')[0].strip()
                 opt_count += 1
                 self.overwrite_lines.append(linum)
-        assert opt_count == len(fileman_opts)
+        assert opt_count == len(self.fman_opts)
         self.__dict__.update(attrs)
 
     def write(self, path=None):
-        assert original_file_contents is not None
+        assert self.original_file_contents is not None
         if not path:
             path = self.original_path
         opt_count = 0
@@ -96,7 +110,7 @@ class FileManager(object):
         for linum, line in enumerate(self.original_contents):
             if linum in self.overwrite_lines:
                 oldval = line.split('!')[0].strip()
-                newval = self.__dict__[fileman_opts[opt_count]]
+                newval = self.__dict__[self.fman_opts[opt_count]]
                 line = line.replace(oldval, newval)
                 opt_count += 1
             self.new_contents.append(line)
