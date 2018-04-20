9adb40b0f60847d975a9bc786bb5daa0991ae00f
DavidChoi76 <yc5ef@virginia.edu>
2018-04-19 20:01:00 -0400

    updated packages info in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(name='pysumma',
       version = '0.1',
-      description = 'This is pySUMMA',
+      description = 'an Object-Oriented Python wrapper for SUMMA model',
       url = 'https://github.com/UW-Hydro/pysumma',
       author = 'YoungDon Choi',
       author_email = 'yc5ef@virginia.edu',
       license = 'BSD-3-Clause',
-      packages=['pysumma'],
+      packages=find_packages(),
       test_suite = 'pysumma.tests')
