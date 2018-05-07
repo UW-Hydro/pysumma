72034e69df209e529a8c0461680326b553d83162
edward <edwardm72@gmail.com>
2018-05-07 00:31:56 -0400

    Added setup.py changes

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(name='pysumma',
       author_email = 'yc5ef@virginia.edu',
       license = 'BSD-3-Clause',
       packages=['pysumma'],
+      install_requires=[
+          'scipy',
+          'xarray',
+          'matplotlib'
+          ],
+      include_package_data=True,
+      dat_files=[('pysumma', 'pysumma/var_lookup.f90')],
       test_suite = 'pysumma.tests')
