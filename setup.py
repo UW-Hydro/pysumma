2484c7fb737e9a351f099e0281bbd52b04f77695
DavidChoi76 <yc5ef@virginia.edu>
2018-05-16 22:50:51 -0400

    updated code and notebooks with edward's PR

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from setuptools import setup, find_packages
 setup(name='pysumma',
       version = '0.1',
       description = 'an Object-Oriented Python wrapper for SUMMA model',
-      url = 'https://github.com/UW-Hydro/pysumma',
+      url = 'https://github.com/uva-hydroinformatics/pysumma',
       author = 'YoungDon Choi',
       author_email = 'yc5ef@virginia.edu',
       license = 'BSD-3-Clause',
-<<<<<<< HEAD
-      packages=find_packages(),
-=======
       packages=['pysumma'],
       install_requires=[
           'scipy',
@@ -18,5 +15,4 @@ setup(name='pysumma',
           ],
       include_package_data=True,
       dat_files=[('pysumma', 'pysumma/var_lookup.f90')],
->>>>>>> 6391d7096f369b2e55c9631b1022a2713860b286
       test_suite = 'pysumma.tests')
