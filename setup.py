a62638a85ec932a582e0f32b366f24a484baa895
Youngdon CHOI <choiyd1115@gmail.com>
2018-05-28 20:59:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(name='pysumma',
       version = '0.1',
       description = 'an Object-Oriented Python wrapper for SUMMA model',
-      url = 'https://github.com/uva-hydroinformatics/pysumma',
+      url = 'https://github.com/DavidChoi76/pysumma.git',
       author = 'YoungDon Choi',
       author_email = 'yc5ef@virginia.edu',
       license = 'BSD-3-Clause',
