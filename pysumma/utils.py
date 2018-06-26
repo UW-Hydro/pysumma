f3f0101244b637b557885c8c431e9548665994b1
DavidChoi76 <yc5ef@virginia.edu>
2018-06-26 17:07:58 -0400

    update notebooks with updated pySUMMA

--- pysumma/utils.py
+++ pysumma/utils.py
@@ -23,3 +23,13 @@ class utils():
         cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(os.path.dirname(testcase_filepath))
         subprocess.run(cmd, shell=True)
         return  os.path.dirname(testcase_filepath)
+
+    def download_executable_lubuntu_hs(save_filepath):
+        resource_id = 'a5dbd5b198c9468387f59f3fefc11e22'
+        hs = HydroShare()
+        hs.getResource(resource_id, destination = save_filepath, unzip = True)
+        resource_name = 'summa-master.zip'
+        executable_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
+        shutil.unpack_archive(executable_filepath, extract_dir=os.path.dirname(executable_filepath))
+        executable = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/summa-master/bin'
+        return  os.path.dirname(executable)
