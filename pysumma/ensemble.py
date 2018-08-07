f9cf9a27d1f29a4d2ba5560ba4ffd8d053cd6910
arbennett <bennett.andr@gmail.com>
2018-08-07 15:04:30 -0700

    Cleanup

--- pysumma/ensemble.py
+++ pysumma/ensemble.py
@@ -1,4 +1,22 @@
-from .Simulation import simulation
+from .Simulation import Simulation
+
+
+class Ensemble(object):
+    '''
+    DecisionEnsembles represent an ensemble of SUMMA
+    configurations based on chainging the decisions file.
+    '''
+
+    executable: str = None
+    simulations: Simulation = []
+
+    def __init__(self):
+        pass
+
+    def execute(self, n_workers: int=1):
+        if not self.executable:
+            raise Exception('No executable set!')
+        raise NotImplementedError()
 
 
 class DecisionEnsemble(Ensemble):
@@ -24,21 +42,4 @@ class ParameterEnsemble(Ensemble):
     '''
     pass
 
-
-class Ensemble(object):
-    '''
-    DecisionEnsembles represent an ensemble of SUMMA
-    configurations based on chainging the decisions file.
-    '''
-
-    executable: str = None
-    simulations: Simulation = []
-
-    def __init__(self):
-        pass
-
-    def execute(self, n_workers: int=1):
-        if not self.executable:
-            raise Exception('No executable set!')
-        raise NotImplementedError()
 
--- pysumma/utils.py
+++ pysumma/utils.py
@@ -4,21 +4,27 @@ from urllib.request import urlretrieve
 import subprocess
 
 
-class utils():
-    def install_test_cases(directory):
-        url = "https://ral.ucar.edu/sites/default/files/public/projects/structure-for-unifying-multiple-modeling-alternatives-summa/summatestcases-2.x.tar.gz"
-        filepath = directory + "summatestcases-2.x.tar.gz"
-        urlretrieve(url, filepath)
-        shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
-        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(directory)
-        subprocess.run(cmd, shell=True)
+def install_test_cases(directory):
+    url = ("https://ral.ucar.edu/sites/default/files/public/projects/"
+           "structure-for-unifying-multiple-modeling-alternatives-summa/"
+           "summatestcases-2.x.tar.gz")
+    filepath = os.path.join(directory, "summatestcases-2.x.tar.gz")
+    urlretrieve(url, filepath)
+    shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
+    cmd = ("cd {}/summaTestCases_2.x/; ./installTestCases_local.sh"
+           .format(directory))
+    subprocess.run(cmd, shell=True)
 
-    def install_test_cases_hs(hs, resource_id, save_filepath):
-        authen = hs
-        authen.getResource(resource_id, destination = save_filepath, unzip = True)
-        resource_name = 'summatestcases-2.x.tar.gz'
-        testcase_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
-        shutil.unpack_archive(testcase_filepath, extract_dir=os.path.dirname(testcase_filepath))
-        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(os.path.dirname(testcase_filepath))
-        subprocess.run(cmd, shell=True)
-        return  os.path.dirname(testcase_filepath)
+def install_test_cases_hs(hs, resource_id, save_filepath):
+    authen = hs
+    authen.getResource(resource_id, destination=save_filepath, unzip=True)
+    resource_name = 'summatestcases-2.x.tar.gz'
+    testcase_filepath = os.path.join(save_filepath, resource_id,
+                                     resource_id, 'data',
+                                     'contents', resource_name)
+    shutil.unpack_archive(testcase_filepath,
+                          extract_dir=os.path.dirname(testcase_filepath))
+    cmd = ("cd {}/summaTestCases_2.x/; ./installTestCases_local.sh"
+           .format(os.path.dirname(testcase_filepath)))
+    subprocess.run(cmd, shell=True)
+    return os.path.dirname(testcase_filepath)
