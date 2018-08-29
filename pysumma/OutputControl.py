355351e2d2e7f25ba6d3d08ee34cae0185e48ab7
arbennett <bennett.andr@gmail.com>
2018-08-28 17:23:43 -0700

    Refactor OutputControl to inherit from base classes

--- pysumma/OutputControl.py
+++ pysumma/OutputControl.py
@@ -1,6 +1,8 @@
+from .Option import BaseOption
+from .Option import OptionContainer
 
 
-class OutputControl(object):
+class OutputControl(OptionContainer):
     """
     The OutputControl object manages what output SUMMA will
     write out.  Each output variable is stored in the `options`
@@ -14,40 +16,18 @@ class OutputControl(object):
         Instantiate the object and populate the
         values from the given filepath.
         """
-        self.original_path = path
-        self.header = []
-        self.options = []
-        self.read(path)
+        super().__init__(path, OutputControlOption)
 
-    def read(self, path):
-        """Read the output control file and load data"""
-        with open(path, 'r') as f:
-            self.original_contents = f.readlines()
-        opt_count = 0
-        for linum, line in enumerate(self.original_contents):
-            if line.startswith('!') and not opt_count:
-                self.header.append(line)
-            elif not line.startswith('!'):
-                opt_count += 1
-                self.options.append(OutputControlOption(
-                    *[l.strip() for l in line.split('|')]))
+    def get_constructor_args(self, line):
+        return line.split('!')[0].split('|')
 
-    def write(self, path=None):
-        """Write the configured output control file"""
-        self.validate()
-        if not path:
-            path = self.original_path
-        with open(path, 'w') as f:
-            f.writelines(self.header)
-            f.writelines((o.__repr__() + '\n' for o in self.options))
-
-    def set_option(self, var=None, period=None, sum=0, instant=1,
+    def set_option(self, name=None, period=None, sum=0, instant=1,
                    mean=0, variance=0, min=0, max=0, mode=0):
         """
         Change or create a new entry in the output control
         """
         try:
-            o = self.get_option(var, strict=True)
+            o = self.get_option(name, strict=True)
             o.period = period
             o.sum = sum
             o.instant = instant
@@ -58,25 +38,11 @@ class OutputControl(object):
             o.mode = mode
         except ValueError:
             self.options.append(
-                    OutputControlOption(var, period, sum, instant,
+                    OutputControlOption(name, period, sum, instant,
                                         mean, variance, min, max, mode))
 
-    def get_option(self, name, strict=False):
-        """Look for an existing option"""
-        for o in self.options:
-            if name == o.var:
-                return o
-        if strict:
-            raise ValueError("Could not find option {}!".format(name))
-        return None
 
-    def validate(self):
-        """Make sure no duplicate options exist"""
-        names = [o.var for o in self.options]
-        assert len(names) == len(set(names)), 'Duplicate options not allowed!'
-
-
-class OutputControlOption(object):
+class OutputControlOption(BaseOption):
 
     def __init__(self, var=None, period=None, sum=0, instant=1,
                  mean=0, variance=0, min=0, max=0, mode=0):
