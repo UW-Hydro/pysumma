c5551bd94458bed449b3b36367891f0ab6558a5f
arbennett <bennett.andr@gmail.com>
2018-12-14 09:10:10 -0800

    Clean up code for pep8 compliance

--- pysumma/simulation.py
+++ pysumma/simulation.py
@@ -1,15 +1,19 @@
 from pysumma.Decisions import Decisions
 from pysumma.Option import Option
 from pysumma.ModelOutput import ModelOutput
-import os              # get directory or filename from filepath
-import subprocess      # run shell script in python
-import shlex           # splite shell script
-import xarray as xr    # create xarray data from summa output (NetCDF file)
+import os
+import subprocess
+import shlex
+import xarray as xr
 import glob
 
-class Simulation:
-    # set filepath parameter as a a directory and a filename of file manager text file
-    def __init__(self, case_data, summa_code = None):
+
+class Simulation(object):
+
+    def __init__(self, case_data, summa_code=None):
+        """
+        Constructor
+        """
         # create self object from file manager text file
         self.filepath = os.path.abspath(case_data)
         self.file_dir = os.path.dirname(self.filepath)
@@ -37,14 +41,19 @@ class Simulation:
         self.output_prefix = FileManagerOption(self, 'output_prefix')
         self.base_dir = case_data.split('/settings')[0]
         # create self object from decision text file
-        self.decision_obj = Decisions(self.base_dir + '/settings/' + self.decision_path.value)
+        self.decision_obj = Decisions(self.base_dir + '/settings/'
+                                      + self.decision_path.value)
 
-        if summa_code == None:
-            self.modeloutput_obj = ModelOutput(self.base_dir + '/settings/' + self.OUTPUT_CONTROL.value, self.base_dir + '/settings/meta/var_lookup.f90')
+        if summa_code is None:
+            self.modeloutput_obj = ModelOutput(
+                self.base_dir + '/settings/' + self.OUTPUT_CONTROL.value,
+                self.base_dir + '/settings/meta/var_lookup.f90')
             self.summa_code = summa_code
         else:
             self.summa_code = summa_code
-            self.modeloutput_obj = ModelOutput(self.base_dir + '/settings/' + self.OUTPUT_CONTROL.value,summa_code + '/build/source/dshare/var_lookup.f90')
+            self.modeloutput_obj = ModelOutput(
+                self.base_dir + '/settings/' + self.OUTPUT_CONTROL.value,
+                summa_code + '/build/source/dshare/var_lookup.f90')
 
     def open_read(self):
         # read filemanager text file
@@ -52,13 +61,15 @@ class Simulation:
             # read every line of filemanager and return as list format
             return f.readlines()
 
-    def execute(self, run_option, run_suffix, specworker_img = None):
+    def execute(self, run_option, run_suffix, specworker_img=None):
         # set run_suffix to distinguish the output name of summa
         self.run_suffix = run_suffix
-        # 'local' run_option runs summa with summa execution file where is in a local computer.
-        if run_option == 'local' :
-            if self.summa_code == None:
-                cmd = "{} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+        # 'local' run_option runs summa with summa execution
+        # file where is in a local computer.
+        if run_option == 'local':
+            if self.summa_code is None:
+                cmd = "{} -p never -s {} -m {}".format(
+                    self.executable, self.run_suffix, self.filepath)
                 # run shell script in python and print output
                 cmd = shlex.split(cmd)
                 p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -67,12 +78,13 @@ class Simulation:
                 if 'FATAL ERROR' in output:
                     raise Exception("SUMMA failed to execute!")
                 # define output file name as sopron version of summa
-                out_file_path = self.output_path.filepath + \
-                                self.output_prefix.value + '_output_' + \
-                                self.run_suffix + '_timestep.nc'
+                out_file_path = (self.output_path.filepath
+                                 + self.output_prefix.value + '_output_'
+                                 + self.run_suffix + '_timestep.nc')
             else:
                 self.executable = self.summa_code + '/bin/summa.exe'
-                cmd = "{} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+                cmd = "{} -p never -s {} -m {}".format(
+                    self.executable, self.run_suffix, self.filepath)
                 # run shell script in python and print output
                 cmd = shlex.split(cmd)
                 p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -81,138 +93,175 @@ class Simulation:
                 if 'FATAL ERROR' in output:
                     raise Exception("SUMMA failed to execute!")
                 # define output file name as sopron version of summa
-                out_file_path = self.output_path.filepath + \
-                                self.output_prefix.value + '_output_' + \
-                                self.run_suffix + '_timestep.nc'
-
-
-        # 'docker_sopron_2018' run_option runs summa with docker hub online, and the version name is "'uwhydro/summa:sopron_2018'.
+                out_file_path = (self.output_path.filepath
+                                 + self.output_prefix.value + '_output_'
+                                 + self.run_suffix + '_timestep.nc')
+        # 'docker_sopron_2018' run_option runs summa with docker hub online,
+        # and the version name is "'uwhydro/summa:sopron_2018'.
         elif run_option == "docker_sopron_2018":
             self.executable = 'uwhydro/summa:sopron_2018'
+            in_path = self.input_path.filepath
+            out_path = self.output_path.filepath
+            base_cmd = "docker run -v {}:{}".format(
+                self.file_dir, self.file_dir)
             if self.file_dir+'/' == self.setting_path.filepath:
-                cmd = "docker run -v {}:{}".format(self.file_dir, self.file_dir) + \
-                      " -v {}:{}".format(self.input_path.filepath, self.input_path.filepath) + \
-                      " -v {}:{}".format(self.output_path.filepath, self.output_path.filepath) + \
-                      " {} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+                cmd = (base_cmd
+                       + " -v {}:{}".format(in_path, in_path)
+                       + " -v {}:{}".format(out_path, out_path)
+                       + " {} -p never -s {} -m {}".format(self.executable,
+                                                           self.run_suffix,
+                                                           self.filepath))
             else:
-                cmd = "docker run -v {}:{}".format(self.file_dir, self.file_dir) + \
-                      " -v {}:{}".format(self.setting_path.filepath, self.setting_path.filepath) + \
-                      " -v {}:{}".format(self.input_path.filepath, self.input_path.filepath) + \
-                      " -v {}:{}".format(self.output_path.filepath, self.output_path.filepath) + \
-                      " {} -p never -s {} -m {}".format(self.executable, self.run_suffix, self.filepath)
+                setting_path = self.setting_path.filepath
+                cmd = (base_cmd
+                       + " -v {}:{}".format(setting_path, setting_path)
+                       + " -v {}:{}".format(in_path, in_path)
+                       + " -v {}:{}".format(out_path, out_path)
+                       + " {} -p never -s {} -m {}".format(self.executable,
+                                                           self.run_suffix,
+                                                           self.filepath))
             # run shell script in python and print output
-            print(cmd)
             cmd = shlex.split(cmd)
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
             output = p.communicate()[0].decode('utf-8')
-            print(output)
             if 'FATAL ERROR' in output:
                 raise Exception("SUMMA failed to execute!")
             # define output file name as sopron version of summa
             if self.output_path.filepath.split('/')[0] == '<BASEDIR>':
-                out_file_path = self.output_path.filepath.split('<BASEDIR>')[1] + \
-                                self.output_prefix.value + '_output_' + \
-                                self.run_suffix + '_timestep.nc'
+                out_file_path = (
+                    self.output_path.filepath.split('<BASEDIR>')[1]
+                    + self.output_prefix.value + '_output_'
+                    + self.run_suffix + '_timestep.nc')
             else:
-                out_file_path = self.output_path.filepath + \
-                                self.output_prefix.value + '_output_' + \
-                                self.run_suffix + '_timestep.nc'
+                out_file_path = (
+                    self.output_path.filepath
+                    + self.output_prefix.value + '_output_'
+                    + self.run_suffix + '_timestep.nc')
 
-        # "specworker" run_option run summa with summa image in docker of HydroShare Jupyter Hub
+        # "specworker" run_option run summa with summa image
+        # in docker of HydroShare Jupyter Hub
         elif run_option == "specworker":
             self.run_suffix = ""
             from specworker import jobs
             # define the image that we want to execute
             if specworker_img == 'cuahsi/summa:master':
-                # save these paths in the env_vars dictionary which will be passed to the model
-                env_vars = {'LOCALBASEDIR': self.base_dir, 'MASTERPATH': self.filepath}
-                # define the location we want to mount these data in the container
+                # save these paths in the env_vars dictionary
+                # which will be passed to the model
+                env_vars = {'LOCALBASEDIR': self.base_dir,
+                            'MASTERPATH': self.filepath}
+                # define the location we want to mount these
+                # data in the container
                 vol_target = '/tmp/summa'
                 # define the base path of the input data for SUMMA
                 vol_source = self.base_dir
                 # run the container with the arguments specified above
-                res = jobs.run(specworker_img, '-x', vol_source, vol_target, env_vars)
+                res = jobs.run(specworker_img, '-x', vol_source,
+                               vol_target, env_vars)
 
-                list = glob.glob(self.base_dir + self.output_path.value.split('>')[1]+"*.nc")
-                output_list = [x for x in list if self.output_prefix.value in x]
+                list = glob.glob(self.base_dir
+                                 + self.output_path.value.split('>')[1]+"*.nc")
+                output_list = [x for x in list
+                               if self.output_prefix.value in x]
                 myoutput_list = ' '.join(output_list[1:])
                 if myoutput_list.count(self.output_prefix.value) > 1:
-                    output_name = self.base_dir + self.output_path.value.split('>')[1] + \
-                                self.output_prefix.value + '_' + \
-                                self.decision_obj.simulStart.value[0:4] + '-' + \
-                                self.decision_obj.simulFinsh.value[0:4] + '_' + \
-                                self.run_suffix + '1.nc'
-                    merge_netcdf = 'ncrcat ' + myoutput_list + ' -O ' + output_name
+                    output_name = (
+                        self.base_dir + self.output_path.value.split('>')[1]
+                        + self.output_prefix.value + '_'
+                        + self.decision_obj.simulStart.value[0:4] + '-'
+                        + self.decision_obj.simulFinsh.value[0:4] + '_'
+                        + self.run_suffix + '1.nc')
+                    merge_netcdf = ('ncrcat ' + myoutput_list
+                                    + ' -O ' + output_name)
                     subprocess.run(merge_netcdf, shell=True)
                     myList = ' '.join(output_list[:])
                     delete_netcdf = 'rm -rf ' + myList
                     subprocess.run(delete_netcdf, shell=True)
 
                 # define output file name as sopron version of summa
-                out_file_path = self.base_dir + self.output_path.value.split('>')[1] + \
-                                self.output_prefix.value + '_' + \
-                                self.decision_obj.simulStart.value[0:4] + '-' + \
-                                self.decision_obj.simulFinsh.value[0:4] + '_' + '1.nc'
+                out_file_path = (
+                    self.base_dir + self.output_path.value.split('>')[1]
+                    + self.output_prefix.value + '_'
+                    + self.decision_obj.simulStart.value[0:4] + '-'
+                    + self.decision_obj.simulFinsh.value[0:4] + '_' + '1.nc')
 
             elif specworker_img == 'cuahsi/summa:sopron':
-                # save these paths in the env_vars dictionary which will be passed to the model
-                env_vars = {'LOCALBASEDIR': self.base_dir, 'MASTERPATH': self.filepath}
-                # define the location we want to mount these data in the container
+                # save these paths in the env_vars dictionary
+                # which will be passed to the model
+                env_vars = {'LOCALBASEDIR': self.base_dir,
+                            'MASTERPATH': self.filepath}
+                # define the location we want to mount
+                # these data in the container
                 vol_target = '/tmp/summa'
                 # define the base path of the input data for SUMMA
                 vol_source = self.base_dir
                 # run the container with the arguments specified above
-                res = jobs.run(specworker_img, '-x', vol_source, vol_target, env_vars)
+                res = jobs.run(specworker_img, '-x', vol_source,
+                               vol_target, env_vars)
 
                 # define output file name as sopron version of summa
-                out_file_path = self.base_dir + '/' + self.output_path.filepath.split('/')[1] + '/' + \
-                                self.output_prefix.value + '_output_' + 'timestep.nc'
+                out_file_path = (
+                    self.base_dir + '/'
+                    + self.output_path.filepath.split('/')[1] + '/'
+                    + self.output_prefix.value + '_output_' + 'timestep.nc')
 
             else:
-                raise ValueError('You need to deinfe the exact SUMMA_image_name')
+                raise ValueError('You need to deinfe the '
+                                 'exact SUMMA_image_name')
 
         else:
-            raise ValueError('No executable defined. Set as "executable" attribute of Simulation or check run_option')
+            raise ValueError('No executable defined. '
+                             'Set as "executable" attribute '
+                             'of Simulation or check run_option')
 
         return xr.open_dataset(out_file_path), out_file_path
 
     def get_output(self, version, output_prefix=None, run_suffix=None):
         if version == "cuahsi_sopron":
-            out_file_path = self.base_dir + '/' + self.output_path.filepath.split('/')[1] + '/' + \
-                            output_prefix + '_output_' + 'timestep.nc'
+            out_file_path = (self.base_dir + '/'
+                             + self.output_path.filepath.split('/')[1] + '/'
+                             + output_prefix + '_output_' + 'timestep.nc')
             xr_output = xr.open_dataset(out_file_path)
         elif version == "cuahsi_master":
-            out_file_path = self.base_dir + self.output_path.value.split('>')[1] + \
-                            output_prefix + '_' + \
-                            self.decision_obj.simulStart.value[0:4] + '-' + \
-                            self.decision_obj.simulFinsh.value[0:4] + '_' + '1.nc'
+            out_file_path = (
+                self.base_dir + self.output_path.value.split('>')[1]
+                + output_prefix + '_'
+                + self.decision_obj.simulStart.value[0:4] + '-'
+                + self.decision_obj.simulFinsh.value[0:4] + '_' + '1.nc')
             xr_output = xr.open_dataset(out_file_path)
         elif version == "docker_sopron":
             if self.output_path.filepath.split('/')[0] == '<BASEDIR>':
-                out_file_path = self.output_path.filepath.split('<BASEDIR>')[1] + \
-                                self.output_prefix.value + '_output_' + \
-                                run_suffix + '_timestep.nc'
+                out_file_path = (
+                    self.output_path.filepath.split('<BASEDIR>')[1]
+                    + self.output_prefix.value + '_output_'
+                    + run_suffix + '_timestep.nc')
             else:
-                out_file_path = self.output_path.filepath + \
-                                self.output_prefix.value + '_output_' + \
-                                run_suffix + '_timestep.nc'
+                out_file_path = (
+                    self.output_path.filepath
+                    + self.output_prefix.value + '_output_'
+                    + run_suffix + '_timestep.nc')
             xr_output = xr.open_dataset(out_file_path)
         else:
-            raise ValueError('You need to write "cuahsi_sopron" or "cuahsi_master"or "docker_sorpon" for version')
+            raise ValueError('You need to write "cuahsi_sopron" or'
+                             ' "cuahsi_master"or "docker_sorpon" for version')
         return xr_output, out_file_path
+
 
 class FileManagerOption(Option):
 
     # key_position is the position in line.split() where the key name is
     # value_position is the position in line.split() where the value is
-    # By default, delimiter=None, but can be set to split each line on different characters
+    # By default, delimiter=None, but can be set to
+    # split each line on different characters
     def __init__(self, parent, name):
-        super().__init__(name, parent, key_position=1, value_position=0, delimiter="!")
+        super().__init__(name, parent, key_position=1,
+                         value_position=0, delimiter="!")
 
-    # get value to change file manager value (value : a line divides by delimiter "!", and directory and filename are value)
+    # get value to change file manager value (value : a line divides
+    # by delimiter "!", and directory and filename are value)
     @property
     def value(self):
         return self.get_value()
+
     # change old value by new value
     @value.setter
     def value(self, new_value):
--- pysumma/utils.py
+++ pysumma/utils.py
@@ -4,77 +4,97 @@ from urllib.request import urlretrieve
 import subprocess
 from hs_restclient import HydroShare
 
-class utils():
 
-    # download SUMMA TestCases from ucar web site, however this is old version of SUMMA TestCases
-    def install_test_cases_summa_web(save_filepath):
-        url = "https://ral.ucar.edu/sites/default/files/public/projects/structure-for-unifying-multiple-modeling-alternatives-summa/summatestcases-2.x.tar.gz"
-        filepath = save_filepath + "summatestcases-2.x.tar.gz"
-        urlretrieve(url, filepath)
-        shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
-        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(save_filepath)
-        subprocess.run(cmd, shell=True)
-
-    # download SUMMA TestCase from HydroShare which is sopron version of SUMMA TestCases
-    def install_test_cases_hs(save_filepath):
-        resource_id = 'a0105d479c334764ba84633c5b9c1c01'
-        hs = HydroShare()
-        hs.getResource(resource_id, destination = save_filepath, unzip = True)
-        resource_name = 'summatestcases-2.x.tar.gz'
-        testcase_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
-        shutil.unpack_archive(testcase_filepath, extract_dir=os.path.dirname(testcase_filepath))
-        cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(os.path.dirname(testcase_filepath))
-        subprocess.run(cmd, shell=True)
-        return  os.path.dirname(testcase_filepath)
-
-    # This method is necessary, when user wants to run SUMMA with local execution file.
-    def download_executable_lubuntu_hs(save_filepath):
-        resource_id = 'a5dbd5b198c9468387f59f3fefc11e22'
-        hs = HydroShare()
-        hs.getResource(resource_id, destination = save_filepath, unzip = True)
-        resource_name = 'summa-master.zip'
-        executable_filepath = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/' + resource_name
-        shutil.unpack_archive(executable_filepath, extract_dir=os.path.dirname(executable_filepath))
-        executable = save_filepath + '/' + resource_id + '/' + resource_id + '/data/contents/summa-master/bin'
-        return  os.path.dirname(executable)
+# download SUMMA TestCases from ucar web site,
+# however this is old version of SUMMA TestCases
+def install_test_cases_summa_web(save_filepath):
+    url = ("https://ral.ucar.edu/sites/default/files/public/projects/"
+           "structure-for-unifying-multiple-modeling-alternatives-summa/"
+           "summatestcases-2.x.tar.gz")
+    filepath = save_filepath + "summatestcases-2.x.tar.gz"
+    urlretrieve(url, filepath)
+    shutil.unpack_archive(filepath, extract_dir=os.path.dirname(filepath))
+    cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(
+        save_filepath)
+    subprocess.run(cmd, shell=True)
 
 
-    def download_model_instance_sopron(resource_id):
-        path = os.getcwd() + '/' + resource_id + '/' + resource_id + '/data/contents/'
-        hs = HydroShare()
-        hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
+# download SUMMA TestCase from HydroShare
+# which is sopron version of SUMMA TestCases
+def install_test_cases_hs(save_filepath):
+    resource_id = 'a0105d479c334764ba84633c5b9c1c01'
+    hs = HydroShare()
+    hs.getResource(resource_id, destination=save_filepath, unzip=True)
+    resource_name = 'summatestcases-2.x.tar.gz'
+    testcase_filepath = (save_filepath + '/' + resource_id + '/'
+                         + resource_id + '/data/contents/' + resource_name)
+    shutil.unpack_archive(testcase_filepath,
+                          extract_dir=os.path.dirname(testcase_filepath))
+    cmd = "cd {}/summaTestCases_2.x/; ./installTestCases_local.sh".format(
+        os.path.dirname(testcase_filepath))
+    subprocess.run(cmd, shell=True)
+    return os.path.dirname(testcase_filepath)
 
-        # unpack the simulation archive and remove unncessary files
-        Model_Instance_Name = os.listdir(path)[1]
-        shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
-        cmd = "rm -rf " + resource_id
-        subprocess.run(cmd, shell=True)
-        return Model_Instance_Name.split('.')[0]
 
-    def download_model_instance(resource_id):
-        path = os.getcwd() + '/' + resource_id + '/' + resource_id + '/data/contents/'
-        hs = HydroShare()
-        hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
+# This method is necessary, when user wants
+# to run SUMMA with local execution file.
+def download_executable_lubuntu_hs(save_filepath):
+    resource_id = 'a5dbd5b198c9468387f59f3fefc11e22'
+    hs = HydroShare()
+    hs.getResource(resource_id, destination=save_filepath, unzip=True)
+    resource_name = 'summa-master.zip'
+    executable_filepath = (save_filepath + '/' + resource_id + '/'
+                           + resource_id + '/data/contents/' + resource_name)
+    shutil.unpack_archive(executable_filepath,
+                          extract_dir=os.path.dirname(executable_filepath))
+    executable = (save_filepath + '/' + resource_id + '/'
+                  + resource_id + '/data/contents/summa-master/bin')
+    return os.path.dirname(executable)
 
-        # unpack the simulation archive and remove unncessary files
-        Model_Instance_Name = os.listdir(path)[0]
-        shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
-        cmd = "rm -rf " + resource_id
-        subprocess.run(cmd, shell=True)
-        return Model_Instance_Name.split('.')[0]
 
-    def download_model_instance_pangeo(resource_id):
-        path = os.getcwd() + '/' + resource_id + '/' + resource_id + '/data/contents/'
-        hs = HydroShare()
-        hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
+def download_model_instance_sopron(resource_id):
+    path = (os.getcwd() + '/' + resource_id + '/'
+            + resource_id + '/data/contents/')
+    hs = HydroShare()
+    hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
 
-        # unpack the simulation archive and remove unncessary files
-        Model_Instance_Name = os.listdir(path)[1]
-        shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
-        cmd = "rm -rf " + resource_id
-        subprocess.run(cmd, shell=True)
-        cmd = 'cd ' + Model_Instance_Name.split('.')[0] +'/; chmod +x ./installTestCases_local.sh'
-        subprocess.run(cmd, shell=True, stderr=subprocess.STDOUT)
-        cmd = 'cd ' + Model_Instance_Name.split('.')[0] + '/; ./installTestCases_local.sh'
-        subprocess.run(cmd, shell=True, stderr=subprocess.STDOUT)
-        return Model_Instance_Name.split('.')[0]
+    # unpack the simulation archive and remove unncessary files
+    Model_Instance_Name = os.listdir(path)[1]
+    shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
+    cmd = "rm -rf " + resource_id
+    subprocess.run(cmd, shell=True)
+    return Model_Instance_Name.split('.')[0]
+
+
+def download_model_instance(resource_id):
+    path = (os.getcwd() + '/' + resource_id + '/'
+            + resource_id + '/data/contents/')
+    hs = HydroShare()
+    hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
+
+    # unpack the simulation archive and remove unncessary files
+    Model_Instance_Name = os.listdir(path)[0]
+    shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
+    cmd = "rm -rf " + resource_id
+    subprocess.run(cmd, shell=True)
+    return Model_Instance_Name.split('.')[0]
+
+
+def download_model_instance_pangeo(resource_id):
+    path = (os.getcwd() + '/' + resource_id + '/'
+            + resource_id + '/data/contents/')
+    hs = HydroShare()
+    hs.getResource(resource_id, destination=os.getcwd(), unzip=True)
+
+    # unpack the simulation archive and remove unncessary files
+    Model_Instance_Name = os.listdir(path)[1]
+    shutil.unpack_archive(path + Model_Instance_Name, extract_dir=os.getcwd())
+    cmd = "rm -rf " + resource_id
+    subprocess.run(cmd, shell=True)
+    cmd = ('cd ' + Model_Instance_Name.split('.')[0]
+           + '/; chmod +x ./installTestCases_local.sh')
+    subprocess.run(cmd, shell=True, stderr=subprocess.STDOUT)
+    cmd = ('cd ' + Model_Instance_Name.split('.')[0]
+           + '/; ./installTestCases_local.sh')
+    subprocess.run(cmd, shell=True, stderr=subprocess.STDOUT)
+    return Model_Instance_Name.split('.')[0]
