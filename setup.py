060ac790432631b6b486d69f2caac58381052456
choi <choi@ubuntu-16.04.4>
2019-11-01 14:23:38 -0400

    update version of pysumma in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pysumma',
-      version='0.0.1',
+      version='0.0.2',
       description='an Object-Oriented Python wrapper for SUMMA model',
       url='https://github.com/uva-hydroinformatics/pysumma.git',
       author='YoungDon Choi',
