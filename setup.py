39e4ae9fcde9fd4c8bf5aed59c556348a5a51de3
arbennett <bennett.andr@gmail.com>
2020-01-30 21:08:03 -0800

    Revert "update pysumma dependence, version etc"
    This reverts commit bd18fa64f72eee829b48c780c6706e8661bc4218.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pysumma',
-      version='0.0.3',
+      version='0.0.1',
       description='an Object-Oriented Python wrapper for SUMMA model',
       url='https://github.com/uva-hydroinformatics/pysumma.git',
       author='YoungDon Choi',
@@ -9,21 +9,12 @@ setup(name='pysumma',
       license='BSD-3-Clause',
       packages=find_packages(),
       install_requires=[
-          'numpy==1.16.4',
-          'xarray==0.12.3',
-          'matplotlib==3.1.0',
-          'pandas==0.24.2',
-          'geopandas==0.5.1',
+          'numpy',
+          'xarray>0.10.9',
+          'matplotlib',
+          'geopandas',
           'shapely',
-          'seaborn==0.9.0',
-          'cartopy==0.17.0',
-          'jupyterthemes==0.20.0',
-          'ipyleaflet==0.11.1',
-          'hs_restclient==1.3.4',
-          'distributed==2.1.0',
-          'bokeh==1.2.0',
-          'fiona==1.8.6',
-          'netcdf4'
+          'seaborn'
           ],
       include_package_data=True,
       test_suite='pysumma.tests')
