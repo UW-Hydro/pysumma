cf3efde757f9e5dba0fd86d2e85adfd452bcaddd
arbennett <bennett.andr@gmail.com>
2020-03-19 00:16:14 -0700

    Implement argument generation for distributed

--- pysumma/distributed.py
+++ pysumma/distributed.py
@@ -4,6 +4,7 @@ from typing import List
 import os
 import pandas as pd
 import time
+import numpy as np
 import xarray as xr
 
 from .simulation import Simulation
@@ -20,7 +21,7 @@ class Distributed(object):
     Currently only supports GRU based parallelization.
     '''
 
-    def __init__(self, executable: str, filemanager: str, initialize: bool=True,
+    def __init__(self, executable: str, filemanager: str,
                  num_workers: int=1, threads_per_worker: int=OMP_NUM_THREADS,
                  chunk_size: int=None, num_chunks: int=None, scheduler: str=None):
         """Initialize a new distributed object"""
@@ -45,19 +46,69 @@ class Distributed(object):
         Generate the arguments that will be used to start multiple
         runs from the base ``self.simulation``
         '''
-        assert not chunk_size and num_chunks, \
+        assert not (chunk_size and num_chunks), \
             "Only specify at most one of `chunk_size` or `num_chunks`!"
         start, stop = 0, 0
+        sim_size = len(self.simulation.local_attributes['gru'])
+        if not (chunk_size or num_chunks):
+            chunk_size = 12
         if chunk_size:
-            # TODO: FIXME: implement
-            chunks = [(start, stop)]
+            sim_truncated = chunk_size * (sim_size // chunk_size)
+            starts = np.linspace(1, sim_truncated, chunk_size).astype(int)
+            stops = np.append(starts[1:] + 1, sim_size)
+            chunks = np.vstack([starts, stops]).T
         elif num_chunks:
-            # TODO: FIXME: implement
-            chunks = [(start, stop)]
+            chunk_size = np.ceil(sim_size / num_chunks).astype(int)
+            starts = np.arange(1, sim_size, chunk_size)
+            stops = np.append(starts[1:] + 1, sim_size)
+            chunks = np.vstack([starts, stops]).T
+        return [{'startGRU': start, 'countGRU': stop - start}
+                for start, stop in chunks]
+
+    def start(self, run_option: str, prerun_cmds: list=None):
+        """
+        Start running the ensemble members.
+
+        Parameters
+        ----------
+        run_option:
+            The run type. Should be either 'local' or 'docker'
+        prerun_cmds:
+            A list of preprocessing commands to run
+        """
+        for n, s in self.simulations.items():
+            # Sleep calls are to ensure writeout happens
+            config = self.configuration[n]
+            self.submissions.append(self._client.submit(
+                _submit, s, n, run_option, prerun_cmds, config))
+
+    def run(self, run_option: str, prerun_cmds=None, monitor: bool=True):
+        """
+        Run the ensemble
+
+        Parameters
+        ----------
+        run_option:
+            Where to run the simulation. Can be ``local`` or ``docker``
+        prerun_cmds:
+            A list of shell commands to run before running SUMMA
+        monitor:
+            Whether to halt operation until runs are complete
+        """
+        self.start(run_option, prerun_cmds)
+        if monitor:
+            return self.monitor()
         else:
-            # TODO: FIXME: implement
-            chunks = [(start, stop)]
-        return chunks
+            return True
+
+    def monitor(self):
+        """
+        Halt computation until submitted simulations are complete
+        """
+        simulations = self._client.gather(self.submissions)
+        for s in simulations:
+            self.simulations[s.run_suffix] = s
+
 
 
 def _submit(s: Simulation, name: str, run_option: str,
