e3c33e75c316c7ecd9d0de4035da88283d44a20c
arbennett <bennett.andr@gmail.com>
2020-07-16 16:44:04 -0700

    Line endings

--- pysumma/evaluation.py
+++ pysumma/evaluation.py
@@ -30,5 +30,32 @@ def kling_gupta_efficiency(sim, obs):
     return kge
 
 
+def decomposed_kling_gupta_efficiency(sim, obs):
+    obs = np.asarray(obs)
+    sim = np.asarray(sim)
+    obs_filtered = obs[~np.isnan(obs)]
+    sim_filtered = sim[~np.isnan(obs)]
+    sim_std = np.std(sim_filtered, ddof=1)
+    obs_std = np.std(obs_filtered, ddof=1)
+    sim_mu = np.mean(sim_filtered)
+    obs_mu = np.mean(obs_filtered)
+    r = np.corrcoef(sim_filtered, obs_filtered)[0, 1]
+    var = sim_std / obs_std
+    bias = sim_mu / obs_mu
+    kge = 1 - np.sqrt((bias-1)**2 + (var-1)**2 + (r-1)**2)
+    return kge, bias, var, r
+
+
+def nash_sutcliffe_efficiency(sim, obs):
+    obs = np.asarray(obs)
+    sim = np.asarray(sim)
+    obs_filtered = obs[~np.isnan(obs)]
+    sim_filtered = sim[~np.isnan(obs)]
+    obs_mu = np.mean(obs_filtered)
+    num = np.sum( (sim_filtered - obs_filtered) ** 2 )
+    den = np.sum( (obs_filtered - obs_mu) ** 2 )
+    return 1 - (num / den)
+
+
 def root_mean_square_error(sim, obs):
     return np.sqrt(mean_squared_error(sim, obs))
